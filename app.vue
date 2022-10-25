d0c117c652ece4a9d542bc3404cde2af3c8d2a73
廖晨竹 <vermouth0226@gmail.com>
2022-10-25 13:55:21 +0800

    add head meta

--- app.vue
+++ app.vue
@@ -1,15 +1,65 @@
 <script setup lang="ts">
+const HEAD_META = [
+  {
+    name: 'description',
+    content: 'Here is Kurt | Follow me on Github @kurt-liao',
+  },
+  // Facebook
+  {
+    name: 'og:url',
+    content: 'https://me.kurtstories.com/',
+  },
+  {
+    name: 'og:type',
+    content: 'website',
+  },
+  {
+    name: 'og:title',
+    content: 'Kurt.me',
+  },
+  {
+    name: 'og:description',
+    content: 'Here is Kurt | Follow me on Github @kurt-liao',
+  },
+  {
+    name: 'og:image',
+    content:
+      'https://raw.githubusercontent.com/kurt-liao/terminal.me/main/public/logo.svg',
+  },
+  // Twitter
+  {
+    name: 'twitter:card',
+    content: 'summary_large_image',
+  },
+  {
+    name: 'twitter:domain',
+    content: 'me.kurtstories.com',
+  },
+  {
+    name: 'twitter:url',
+    content: 'https://me.kurtstories.com/',
+  },
+  {
+    name: 'twitter:title',
+    content: 'Kurt.me',
+  },
+  {
+    name: 'twitter:description',
+    content: 'Here is Kurt | Follow me on Github @kurt-liao',
+  },
+  {
+    name: 'twitter:image',
+    content:
+      'https://raw.githubusercontent.com/kurt-liao/terminal.me/main/public/logo.svg',
+  },
+]
+
 useHead({
   htmlAttrs: {
     lang: 'en',
   },
   title: 'Kurt.me',
-  meta: [
-    {
-      name: 'description',
-      content: 'Here is Kurt | Follow me on Github @kurt-liao',
-    },
-  ],
+  meta: HEAD_META,
 })
 </script>
 
